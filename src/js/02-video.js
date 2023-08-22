6434501dad8f49082d7d248eb37a0ce0e13dfd6a
PiganS <pigggggan@gmail.com>
2023-08-22 15:06:14 +0300

    second-commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframeRef = document.querySelector('#vimeo-player');
+const player = new Player(iframeRef);
+
+player.on('timeupdate', throttle(saveCurrentBrowsingTime, 1000));
+
+function saveCurrentBrowsingTime(event) {
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(event.seconds)
+  );
+}
+
+try {
+  const savedTime = JSON.parse(
+    localStorage.getItem('videoplayer-current-time')
+  );
+  player.setCurrentTime(savedTime);
+} catch (error) {
+  console.error(error.name, error.message);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  emailInp: document.querySelector('input[name="email"]'),
+  messageInp: document.querySelector('textarea[name="message"]'),
+};
+
+const handleInput = () => {
+  const formData = {
+    email: refs.emailInp.value.trim(),
+    message: refs.messageInp.value.trim(),
+  };
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+};
+
+refs.emailInp.addEventListener('input', throttle(handleInput, 500));
+refs.messageInp.addEventListener('input', throttle(handleInput, 500));
+
+const loadFormData = () => {
+  const savedData = localStorage.getItem('feedback-form-state');
+  if (savedData) {
+    const formData = JSON.parse(savedData);
+    refs.emailInp.value = formData.email || '';
+    refs.messageInp.value = formData.message || '';
+  }
+};
+
+window.addEventListener('load', loadFormData);
+refs.form.addEventListener('submit', formSabmit);
+
+function formSabmit(evt) {
+  evt.preventDefault();
+  const formData = {
+    email: refs.emailInp.value.trim(),
+    message: refs.messageInp.value.trim(),
+  };
+  console.log('Form Data:', formData);
+
+  localStorage.removeItem('feedback-form-state');
+  refs.emailInp.value = '';
+  refs.messageInp.value = '';
+}
